678185d735a9031e2d6d57f99b92bafbd89730f9
giack-dev <github.giack@gmail.com>
2024-03-06 11:41:42 +0100

    renamed db.py to init_db.py, modified db SQL code

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    name TEXT NOT NULL
-    password TEXT NOT NULL
+    name TEXT NOT NULL,
+    password TEXT NOT NULL,
     favorite_city TEXT
 );
