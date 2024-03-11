477de2b826677402b63d9ddc3a2b97d52ced5202
giack-dev <github.giack@gmail.com>
2024-03-11 09:45:42 +0100

    reformatted code, added a line to the .gitignore

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,19 +1,22 @@
 from dotenv import dotenv_values
 import sqlite3
 
-connection: sqlite3.Connection = sqlite3.connect('./backend/users.db')
+connection: sqlite3.Connection = sqlite3.connect("./backend/users.db")
 config_values = dotenv_values(".env")
 
-with open('./backend/users.sql') as f:
+with open("./backend/users.sql") as f:
     connection.executescript(f.read())
 
 db_cursor: sqlite3.Cursor = connection.cursor()
 
-db_cursor.execute("INSERT INTO users (name, password, favorite_city) VALUES (?, ?, ?)", (
-                                                                                        config_values.get('ADMIN_USERNAME'), 
-                                                                                        config_values.get('ADMIN_PASSWORD'), 
-                                                                                        config_values.get('ADMIN_FAVORITE_CITY')
-                                                                                        ))
+db_cursor.execute(
+    "INSERT INTO users (name, password, favorite_city) VALUES (?, ?, ?)",
+    (
+        config_values.get("ADMIN_USERNAME"),
+        config_values.get("ADMIN_PASSWORD"),
+        config_values.get("ADMIN_FAVORITE_CITY"),
+    ),
+)
 
 connection.commit()
 connection.close()
