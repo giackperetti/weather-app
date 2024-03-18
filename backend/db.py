81046cdc39c12c28e90666aadad3028b84d5360b
giack-dev <github.giack@gmail.com>
2024-03-18 09:29:47 +0100

    created edit_user method to edit an existing user's attributes

--- backend/db.py
+++ backend/db.py
@@ -8,10 +8,10 @@ class DB_Interactions:
         """
         __init__: Method that constructs an oject of class DB_Interactions, creating a connection to the database and creating a cursorto browse the daatbase's conents
         """
-        self.connection = self.create_connection()
+        self.connection = self.handle_connection()
         self.db_cursor: sqlite3.Cursor = self.connection.cursor()
 
-    def create_connection(self) -> sqlite3.Connection:
+    def handle_connection(self) -> sqlite3.Connection:
         """
         create_connection: Method that handles the creation of the connection to the database
 
@@ -52,6 +52,7 @@ class DB_Interactions:
                 user.set_id(row["id"])
                 user.set_creation_date(row["creation_date"])
             self.connection.commit()
+            self.connection.close()
             adding_success = True
             status = "User added succesfully"
         except Exception as e:
@@ -64,8 +65,8 @@ class DB_Interactions:
         """
         delete_user: Method that deletes an exising user and saves it to the database
 
-        :param User user: The user that has to be added
-        :return Tuple[str, bool]: the first value in the tuple is a string that contatins a message about the creation of the user, the second value is a boolean value that is True if the user is added correctly and False if there is a problem
+        :param User user: The user that has to be deleted
+        :return Tuple[str, bool]: the first value in the tuple is a string that contatins a message about the deletion of the user, the second value is a boolean value that is True if the user is deleted correctly and False if there is a problem
         """
 
         deletion_success: bool = None
@@ -74,6 +75,8 @@ class DB_Interactions:
 
         try:
             self.db_cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+            self.connection.commit()
+            self.connection.close()
             deletion_success = True
             status = "User deleted succesfully"
         except Exception as e:
@@ -81,3 +84,38 @@ class DB_Interactions:
             status = f"There was an error: {e}"
 
         return status, deletion_success
+    
+    def edit_user(self, user: User, new_username: str, new_password: str, new_favorite_city: str) -> Tuple[str, bool]:
+        """
+        edit_user: Method that edits an exising user and saves it to the database
+
+        :param User user: The user that has to be edited
+        :return Tuple[str, bool]: the first value in the tuple is a string that contatins a message about the editing of the user, the second value is a boolean value that is True if the user is edited correctly and False if there is a problem
+        """
+
+        editing_success: bool = None
+        status: str = "User's "
+        current_username: str = user.get_username()
+        current_user_password: str = user.get_password()
+        current_user_city: str = user.get_favorite_city()
+
+        try:
+            if new_username != "":
+                current_username = user.set_username(new_username)
+                status += "username, "
+
+            if new_password != "":
+                current_user_password = user.set_password(new_password)
+                status += "password, "
+            if new_favorite_city != "":
+                current_user_city = user.set_favorite_city(new_favorite_city)
+                status += "favorite city, "
+
+            editing_success = True
+            status += "edited succesfully"
+
+        except Exception as e:
+            editing_success = False
+            status = f"There was an error: {e}"
+
+        return status, editing_success
